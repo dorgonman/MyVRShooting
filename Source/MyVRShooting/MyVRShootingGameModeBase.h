52894152bb47d500ee602feb606dfe0be0edbe02
dorgon <dorgon.chang@mai.ai>
2017-06-03 11:21:12 -0700

    Initial commit

---
+++ Source/MyVRShooting/MyVRShootingGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MyVRShootingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYVRSHOOTING_API AMyVRShootingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
